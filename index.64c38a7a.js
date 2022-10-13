e441b1617cacb18260fa472f1c3bcfb92ae81405
YuriyChernyakov <100133751+YuriyChernyakov@users.noreply.github.com>
2022-10-13 15:18:19 +0000

    Deploying to gh-pages from @ YuriyChernyakov/goit-markup-command-project@9ef2ea9934fd697997bf392d3ce62d79da3c970a 🚀

---
+++ index.64c38a7a.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[modal-locations-open]"),closeModalBtn:document.querySelector("[modal-locations-close]"),modal:document.querySelector("[modal-locations]")};function e(){o.modal.classList.toggle("is-hiddden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.64c38a7a.js.map
